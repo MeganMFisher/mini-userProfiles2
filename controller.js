4e326d8f6e9d9661da7e43b3e5acb88b5be84383
MeganMFisher <meganmichelle318@gmail.com>
2017-02-08 12:26:54 -0700

    Completed

--- controller.js
+++ controller.js
@@ -1,10 +1,36 @@
 angular.module('userProfiles').controller('MainController', function($scope, mainService) {
+//   $scope.getUsers = function() {
+//     mainService.getUsers().then(function(response) {
+//       $scope.users = response.data.data;
+//     });
+//   }
+//   $scope.getUsers();
+// });
 
-  $scope.getUsers = function() {
-  	$scope.users = mainService.getUsers();
+$scope.currentPage = 2;
+
+$scope.getUsers = function () {
+  var promise = mainService.getUsers($scope.currentPage);
+  promise.then(function(usersObj){
+    $scope.users = usersObj.data;
+    $scope.total_pages = usersObj.total_pages;
+  })
+}
+
+$scope.prev = function(){
+  if($scope.currentPage > 1){
+    $scope.currentPage--;
+
   }
-
-
   $scope.getUsers();
+}
 
+$scope.next = function(){
+  if($scope.currentPage <$scope.total_pages){
+  $scope.currentPage++;
+  }
+  $scope.getUsers();
+}
+
+$scope.getUsers();
 });
